0bbdc61491cf04d180c6841e2e03329e934728c2
andrea longhi <andrea@spaghetticode.it>
2019-04-29 17:18:24 +0200

    Add basic styles for V3 HTML fields
    While there's some room for styling V3 Elements directly via javascript, the
    rest of the styles must be added to the parent element of each Stripe element.
    
    `solidus_stripe.scss` includes some basic styles that may show how to futher
    customize Stripe fields appearance.

--- lib/generators/solidus_stripe/install/install_generator.rb
+++ lib/generators/solidus_stripe/install/install_generator.rb
@@ -3,6 +3,10 @@ module SolidusStripe
     class InstallGenerator < Rails::Generators::Base
       class_option :auto_run_migrations, type: :boolean, default: false
 
+      def add_stylesheets
+        inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", " *= require spree/frontend/solidus_stripe\n", before: '*/', verbose: true
+      end
+
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=solidus_stripe'
       end
