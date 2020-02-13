fc3d6856e01a9714412a1a7272458af5228b13ea
andrea longhi <andrea@spaghetticode.it>
2020-02-13 11:09:57 +0100

    Add PaymentRequestController
    This controller manages requests from the frontend payment request
    when the cart page payment is enabled.
    
    It provides the shipping methods and their cost to be shown to the
    customer and an endpoint that process the order with the customer
    and payment information received from the FE payment request, after
    the customer selects their address and the shipping method for the
    order.

---
+++ app/controllers/solidus_stripe/payment_request_controller.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module SolidusStripe
+  class PaymentRequestController < Spree::BaseController
+    include Spree::Core::ControllerHelpers::Order
+
+    def shipping_rates
+      rates = SolidusStripe::ShippingRatesService.new(
+        current_order,
+        spree_current_user,
+        params[:shipping_address]
+      ).call
+
+      if rates.any?
+        render json: { success: true, shipping_rates: rates }
+      else
+        render json: { success: false, error: 'No shipping method available for that address' }, status: 500
+      end
+    end
+
+    def update_order
+      current_order.restart_checkout_flow
+
+      address = SolidusStripe::AddressFromParamsService.new(
+        params[:shipping_address],
+        spree_current_user
+      ).call
+
+      if address.valid?
+        SolidusStripe::PrepareOrderForPaymentService.new(address, self).call
+
+        if current_order.payment?
+          render json: { success: true }
+        else
+          render json: { success: false, error: 'Order not ready for payment. Try manual checkout.' }, status: 500
+        end
+      else
+        render json: { success: false, error: address.errors.full_messages.to_sentence }, status: 500
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Spree::Core::Engine.routes.draw do
   post '/stripe/confirm_payment', to: 'stripe#confirm_payment'
+
+  # payment request routes:
+  post '/stripe/shipping_rates', to: '/solidus_stripe/payment_request#shipping_rates'
+  post '/stripe/update_order', to: '/solidus_stripe/payment_request#update_order'
 end
