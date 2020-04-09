dba3c5c7986f4af506b43d49200a26d6b0ee74b0
andrea longhi <andrea@spaghetticode.it>
2020-04-09 11:09:58 +0200

    Add CreateIntentsOrderService
    This service encapsulates all the logic for creating a Solidus
    payment starting from a Stripe Payment Intent object.
    
    It also updates the Payment Intent description with Solidus
    payment number, so it's much easier to match them on Stripe
    dashboard.

---
+++ app/models/solidus_stripe/create_intents_order_service.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+module SolidusStripe
+  class CreateIntentsOrderService
+    attr_reader :intent, :stripe, :controller
+
+    delegate :request, :current_order, :params, to: :controller
+
+    def initialize(intent, stripe, controller)
+      @intent, @stripe, @controller = intent, stripe, controller
+    end
+
+    def call
+      payment = create_payment
+      description = "Solidus Order ID: #{payment.gateway_order_identifier}"
+      stripe.update_intent(nil, response['id'], nil, description: description)
+    end
+
+    private
+
+    def create_payment
+      Spree::OrderUpdateAttributes.new(
+        current_order,
+        payment_params,
+        request_env: request.headers.env
+      ).apply
+
+      Spree::Payment.find_by(response_code: response['id']).tap do |payment|
+        payment.update!(state: :pending)
+      end
+    end
+
+    def payment_params
+      card = response['charges']['data'][0]['payment_method_details']['card']
+      address_attributes = form_data['payment_source'][stripe.id.to_s]['address_attributes']
+
+      {
+        payments_attributes: [{
+          payment_method_id: stripe.id,
+          amount: current_order.total,
+          response_code: response['id'],
+          source_attributes: {
+            month: card['exp_month'],
+            year: card['exp_year'],
+            cc_type: card['brand'],
+            gateway_payment_profile_id: response['payment_method'],
+            last_digits: card['last4'],
+            name: current_order.bill_address.full_name,
+            address_attributes: address_attributes
+          }
+        }]
+      }
+    end
+
+    def response
+      intent.params
+    end
+
+    def form_data
+      Rack::Utils.parse_nested_query(params[:form_data])
+    end
+  end
+end
