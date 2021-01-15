3ee5009a2e4a7da26bd188350e5a64b81ea56ed7
Filippo Liverani <dev@mailvore.com>
2021-01-15 17:54:00 +0100

    Fix card input in feature specs

--- lib/solidus_stripe/testing_support/card_input_helper.rb
+++ lib/solidus_stripe/testing_support/card_input_helper.rb
@@ -9,16 +9,26 @@ module SolidusCardInputHelper
 
     if preferred_v3_elements || preferred_v3_intents
       within_frame find('#card_number iframe') do
-        card[:number].split('').each { |n| find_field('cardnumber').native.send_keys(n) }
+        fill_in_number("cardnumber", card)
       end
       within_frame(find '#card_cvc iframe') { fill_in 'cvc', with: card[:code] }
       within_frame(find '#card_expiry iframe') do
-        "#{card[:exp_month]}#{card[:exp_year].last(2)}".split('').each { |n| find_field('exp-date').native.send_keys(n) }
+        fill_in_expiration("exp-date", card)
       end
     else
-      fill_in "Card Number", with: card[:number]
+      fill_in_number("Card Number", card)
       fill_in "Card Code", with: card[:code]
-      fill_in "Expiration", with: "#{card[:exp_month]} / #{card[:exp_year]}"
+      fill_in_expiration("Expiration", card)
     end
+  end
+
+  private
+
+  def fill_in_number(field_name, card)
+    card[:number].split('').each { |n| find_field(field_name).native.send_keys(n) }
+  end
+
+  def fill_in_expiration(field_name, card)
+    "#{card[:exp_month]}#{card[:exp_year].last(2)}".split('').each { |n| find_field(field_name).native.send_keys(n) }
   end
 end
