e2a55fdd529ba2cc31f458f1cf0403355eb2642d
Elia Schito <elia@schito.me>
2023-01-16 19:33:38 +0100

    Attach the stripe payment source to a payment intent id

--- app/models/solidus_stripe/payment_source.rb
+++ app/models/solidus_stripe/payment_source.rb
@@ -4,5 +4,17 @@ require 'stripe'
 
 module SolidusStripe
   class PaymentSource < ::Spree::PaymentSource
+    alias_attribute :gateway_payment_profile_id, :stripe_payment_intent_id
+    alias_attribute :gateway_payment_profile_id=, :stripe_payment_intent_id=
+
+    def payment_intent
+      return nil if stripe_payment_intent_id.blank?
+
+      payment_intent, _response = payment_method.gateway.client.request do
+        Stripe::PaymentIntent.retrieve(stripe_payment_intent_id)
+      end
+
+      payment_intent
+    end
   end
 end
