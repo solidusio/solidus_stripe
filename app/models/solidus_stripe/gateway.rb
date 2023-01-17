467f23580950a13f12f01582792a52c434cd021a
Elia Schito <elia@schito.me>
2023-01-17 18:25:27 +0100

    Add a basic gateway implementation

--- app/models/solidus_stripe/gateway.rb
+++ app/models/solidus_stripe/gateway.rb
@@ -2,8 +2,11 @@
 
 require 'stripe'
 
-# rubocop:disable Lint/UnusedMethodArgument, Style/MethodCallWithoutArgsParentheses
+# Documentation about separate authorization and capture
+# - https://stripe.com/docs/payments/accept-a-payment?platform=web&ui=checkout#auth-and-capture
+# - https://stripe.com/docs/charges/placing-a-hold
 module SolidusStripe
+  # rubocop:disable Lint/UnusedMethodArgument
   class Gateway
     def initialize(options)
       # Cannot use kwargs because of how the Gateway is initialized by Solidus.
@@ -13,31 +16,89 @@ module SolidusStripe
       @options = options
     end
 
+    attr_reader :client
+
     # Authorizes a certain amount on the provided payment source.
-    def authorize(money, source, options = {})
-      ActiveMerchant::Billing::Response.new()
+    def authorize(amount_in_cents, source, options = {})
+      payment_intent_options = options[:payment_intent_options].dup.to_h
+      payment_intent_options[:capture_method] = "manual"
+      purchase(amount_in_cents, source, options.merge(payment_intent_options: payment_intent_options))
     end
 
     # Captures a certain amount from a previously authorized transaction.
-    def capture(money, transaction_id, options = {})
-      ActiveMerchant::Billing::Response.new()
+    # Ref: https://stripe.com/docs/api/payment_intents/capture#capture_payment_intent
+    # Ref: https://stripe.com/docs/payments/capture-later
+    def capture(amount_in_cents, _transaction_id, options = {})
+      payment = options[:originator] or raise ArgumentError, "please provide a payment with the :originator option"
+      payment_intent_id = payment.source.stripe_payment_intent_id
+
+      raise ArgumentError, "missing transaction_id" unless payment_intent_id
+
+      unless payment_intent_id.start_with?('pi_')
+        raise ArgumentError, "the payment-intent id has the wrong format"
+      end
+
+      payment_intent, _response = client.request { Stripe::PaymentIntent.capture(payment_intent_id) }
+
+      ActiveMerchant::Billing::Response.new(
+        true, "Capture was successful", { 'stripe_payment_intent' => payment_intent.to_json }, {}
+      )
+    rescue Stripe::InvalidRequestError => e
+      ActiveMerchant::Billing::Response.new(
+        false, e.to_s, { 'json_response' => e.response.to_json }, {}
+      )
     end
 
     # Authorizes and captures a certain amount on the provided payment source.
-    def purchase(money, source, options = {})
-      ActiveMerchant::Billing::Response.new()
+    def purchase(amount_in_cents, source, options = {})
+      currency = options.fetch(:currency)
+
+      # Charge the Customer instead of the card:
+      payment_intent, _response = client.request do
+        Stripe::PaymentIntent.create({
+          amount: MoneyToStripeAmountConverter.to_stripe_amount(amount_in_cents, currency),
+          currency: currency,
+          **options[:payment_intent_options].to_h
+        })
+      end
+
+      ActiveMerchant::Billing::Response.new(
+        true, "PaymentIntent was created successfully", { 'stripe_payment_intent' => payment_intent.to_json }, {}
+      )
     end
 
     # Voids a previously authorized transaction, releasing the funds that are on hold.
     # The source parameter is only needed for payment gateways that support payment profiles.
-    def void(transaction_id, source, options = {})
-      ActiveMerchant::Billing::Response.new()
+    def void(_transaction_id, source, options = {})
+      payment_intent, _response = client.request do
+        Stripe::PaymentIntent.cancel(source.stripe_payment_intent_id)
+      end
+
+      ActiveMerchant::Billing::Response.new(
+        true, "PaymentIntent was canceled successfully", { 'stripe_payment_intent' => payment_intent.to_json }, {}
+      )
+    rescue Stripe::InvalidRequestError => e
+      ActiveMerchant::Billing::Response.new(
+        false, e.to_s, { 'json_response' => e.response.to_json }, {}
+      )
     end
 
     # Refunds the provided amount on a previously captured transaction.
     # The source parameter is only needed for payment gateways that support payment profiles.
-    def credit(money, source, transaction_id, options = {})
-      ActiveMerchant::Billing::Response.new()
+    def credit(amount_in_cents, source, transaction_id, options = {})
+      currency = options.fetch(:currency)
+      source ||= options[:originator].source
+
+      refund, _response = client.request do
+        Stripe::Refund.create(
+          amount: MoneyToStripeAmountConverter.to_stripe_amount(amount_in_cents, currency),
+          payment_intent: source.stripe_payment_intent_id,
+        )
+      end
+
+      ActiveMerchant::Billing::Response.new(
+        true, "PaymentIntent was refunded successfully", { 'stripe_refund' => refund.to_json }, {}
+      )
     end
 
     module MoneyToStripeAmountConverter
@@ -104,5 +165,5 @@ module SolidusStripe
       end
     end
   end
+  # rubocop:enable Lint/UnusedMethodArgument
 end
-# rubocop:enable Style/MethodCallWithoutArgsParentheses, Lint/UnusedMethodArgument
--- spec/models/solidus_stripe/gateway_spec.rb
+++ spec/models/solidus_stripe/gateway_spec.rb
@@ -6,7 +6,7 @@ require 'solidus_stripe_spec_helper'
 RSpec.describe SolidusStripe::Gateway do
   describe SolidusStripe::Gateway::MoneyToStripeAmountConverter do
     describe '.to_stripe_amount' do
-      it 'converts to the fractional expected by stripe' do # rubocop:disable RSpec/MultipleExpectations
+      it 'converts to the fractional expected by stripe' do
         (described_class::ZERO_DECIMAL_CURRENCIES - %w[MGA]).each do |currency|
           expect([currency, described_class.to_stripe_amount(12345, currency).to_i]).to eq([currency, 12345])
         end
@@ -25,6 +25,108 @@ RSpec.describe SolidusStripe::Gateway do
         expect(['UGX', described_class.to_stripe_amount(12_3450_00, 'UGX').to_i]).to eq(['UGX', 123_450_00])
         expect(['TWD', described_class.to_stripe_amount(1_2345_00, 'TWD').to_i]).to eq(['TWD', 12_345_00])
       end
+    end
+  end
+
+  describe '#authorize' do
+    it 'uses a manual capture method' do
+      gateway = build(:stripe_payment_method).gateway
+      source = instance_double(SolidusStripe::PaymentSource)
+      payment_intent = instance_double(Stripe::PaymentIntent, to_json: '{foo: "pi_123"}')
+      allow(Stripe::PaymentIntent).to receive(:create).and_return(payment_intent)
+
+      result = gateway.authorize(123_45, source, currency: 'USD')
+
+      expect(Stripe::PaymentIntent).to have_received(:create).with(
+        amount: 123_45,
+        currency: 'USD',
+        capture_method: 'manual',
+      )
+      expect(result.params['stripe_payment_intent']).to eq('{foo: "pi_123"}')
+    end
+  end
+
+  describe '#capture' do
+    it 'captures a pre-authorized amount' do
+      gateway = build(:stripe_payment_method).gateway
+      source = instance_double(SolidusStripe::PaymentSource, stripe_payment_intent_id: 'pi_123')
+      payment = instance_double(Spree::Payment, source: source)
+      payment_intent = instance_double(Stripe::PaymentIntent, to_json: '{foo: "pi_123"}')
+      allow(Stripe::PaymentIntent).to receive(:capture).and_return(payment_intent)
+
+      result = gateway.capture(123_45, nil, originator: payment)
+
+      expect(Stripe::PaymentIntent).to have_received(:capture).with('pi_123')
+      expect(result.params['stripe_payment_intent']).to eq('{foo: "pi_123"}')
+    end
+  end
+
+  describe '#purchase' do
+    it 'authorizes and captures in a single operation' do
+      gateway = build(:stripe_payment_method).gateway
+      source = instance_double(SolidusStripe::PaymentSource)
+      payment_intent = instance_double(Stripe::PaymentIntent, to_json: '{foo: "pi_123"}')
+      allow(Stripe::PaymentIntent).to receive(:create).and_return(payment_intent)
+
+      result = gateway.purchase(123_45, source, currency: 'USD')
+
+      expect(Stripe::PaymentIntent).to have_received(:create).with(
+        amount: 123_45,
+        currency: 'USD',
+      )
+      expect(result.params['stripe_payment_intent']).to eq('{foo: "pi_123"}')
+    end
+  end
+
+  describe '#void' do
+    it 'voids a payment that hasn not been captured yet' do
+      gateway = build(:stripe_payment_method).gateway
+      source = instance_double(SolidusStripe::PaymentSource, stripe_payment_intent_id: 'pi_123')
+      payment_intent = instance_double(Stripe::PaymentIntent, to_json: '{foo: "pi_123"}')
+      allow(Stripe::PaymentIntent).to receive(:cancel).and_return(payment_intent)
+      transaction_id = instance_double(String)
+
+      result = gateway.void(transaction_id, source)
+
+      expect(Stripe::PaymentIntent).to have_received(:cancel).with(
+        'pi_123'
+      )
+      expect(result.params['stripe_payment_intent']).to eq('{foo: "pi_123"}')
+    end
+  end
+
+  describe '#credit' do
+    it 'refunds when provided a source' do
+      gateway = build(:stripe_payment_method).gateway
+      source = instance_double(SolidusStripe::PaymentSource, stripe_payment_intent_id: 'pi_123')
+      refund = instance_double(Stripe::Refund, to_json: '{foo: "re_123"}')
+      allow(Stripe::Refund).to receive(:create).and_return(refund)
+      transaction_id = instance_double(String)
+
+      result = gateway.credit(123_45, source, transaction_id, currency: 'USD')
+
+      expect(Stripe::Refund).to have_received(:create).with(
+        payment_intent: 'pi_123',
+        amount: 123_45,
+      )
+      expect(result.params['stripe_refund']).to eq('{foo: "re_123"}')
+    end
+
+    it 'refunds when provided an originator payment' do
+      gateway = build(:stripe_payment_method).gateway
+      source = instance_double(SolidusStripe::PaymentSource, stripe_payment_intent_id: 'pi_123')
+      payment = instance_double(Spree::Payment, source: source)
+      refund = instance_double(Stripe::Refund, to_json: '{foo: "re_123"}')
+      allow(Stripe::Refund).to receive(:create).and_return(refund)
+      transaction_id = instance_double(String)
+
+      result = gateway.credit(123_45, nil, transaction_id, currency: 'USD', originator: payment)
+
+      expect(Stripe::Refund).to have_received(:create).with(
+        payment_intent: 'pi_123',
+        amount: 123_45,
+      )
+      expect(result.params['stripe_refund']).to eq('{foo: "re_123"}')
     end
   end
 end
