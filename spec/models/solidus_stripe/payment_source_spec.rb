8f42dce9ad65a06aa50152caf062c881a64dae6b
Elia Schito <elia@schito.me>
2023-02-13 15:27:33 +0100

    Add a spec for the payment source factory

--- spec/models/solidus_stripe/payment_source_spec.rb
+++ spec/models/solidus_stripe/payment_source_spec.rb
@@ -3,4 +3,7 @@
 require 'solidus_stripe_spec_helper'
 
 RSpec.describe SolidusStripe::PaymentSource, type: :model do
+  it 'has a working factory' do
+    expect(create(:stripe_payment_source)).to be_valid
+  end
 end
