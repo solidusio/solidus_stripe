98031754e27571a3c7f221b0f397a2dc635eb5cd
Elia Schito <elia@schito.me>
2023-02-23 18:09:23 +0100

    Wrap payment method methods into groups/concerns

--- app/models/solidus_stripe/payment_method.rb
+++ app/models/solidus_stripe/payment_method.rb
@@ -7,44 +7,48 @@ module SolidusStripe
 
     validates :available_to_admin, inclusion: { in: [false] }
 
-    def partial_name
-      "stripe"
-    end
-
-    alias cart_partial_name partial_name
-    alias product_page_partial_name partial_name
-
-    def payment_source_class
-      PaymentSource
-    end
-
-    def gateway_class
-      Gateway
-    end
-
-    def create_profile(payment)
-      payment_intent = payment.source.payment_intent
-
-      if payment_intent && payment_intent.customer.blank?
-        payment.payment_method.gateway.request do
-          payment_intent.customer = Stripe::Customer.new email: payment.order.email
-          payment_intent.customer.save
-          payment_intent.save
-        end
+    concerning :Configuration do
+      def partial_name
+        "stripe"
       end
 
-      self
+      alias cart_partial_name partial_name
+      alias product_page_partial_name partial_name
+
+      def payment_source_class
+        PaymentSource
+      end
+
+      def gateway_class
+        Gateway
+      end
     end
 
-    def payment_profiles_supported?
-      true
-    end
+    concerning :Payment do
+      def create_profile(payment)
+        payment_intent = payment.source.payment_intent
 
-    def stripe_dashboard_url(payment)
-      intent_id = payment.source.stripe_payment_intent_id
-      path_prefix = '/test' if preferred_test_mode
+        if payment_intent && payment_intent.customer.blank?
+          payment.payment_method.gateway.request do
+            payment_intent.customer = Stripe::Customer.new email: payment.order.email
+            payment_intent.customer.save
+            payment_intent.save
+          end
+        end
 
-      "https://dashboard.stripe.com#{path_prefix}/payments/#{intent_id}"
+        self
+      end
+
+      def payment_profiles_supported?
+        true
+      end
+
+      def stripe_dashboard_url(payment)
+        intent_id = payment.source.stripe_payment_intent_id
+        path_prefix = '/test' if preferred_test_mode
+
+        "https://dashboard.stripe.com#{path_prefix}/payments/#{intent_id}"
+      end
     end
   end
 end
