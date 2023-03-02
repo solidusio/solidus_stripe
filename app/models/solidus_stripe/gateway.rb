e4ead6a031fd28366468652516e2440896d46565
Elia Schito <elia@schito.me>
2023-03-02 15:57:00 +0100

    Add Gateway#authorize
    This is used to authorize from existing payment sources attached to
    the user wallet.
    
    This partially reverts commit dcfa63516445d3d10dc5c53d51c1ed3be9c14322.

--- app/models/solidus_stripe/gateway.rb
+++ app/models/solidus_stripe/gateway.rb
@@ -29,6 +29,21 @@ module SolidusStripe
 
     attr_reader :client
 
+    # Authorizes a certain amount on the provided payment source.
+    #
+    # @see #purchase
+    def authorize(amount_in_cents, source, options = {})
+      stripe_payment_method = source.stripe_payment_method
+
+      payment_intent_options = options[:payment_intent_options].dup.to_h
+      payment_intent_options[:capture_method] = "manual"
+      payment_intent_options[:confirm] = true
+      payment_intent_options[:payment_method] = stripe_payment_method.id
+      payment_intent_options[:customer] = stripe_payment_method.customer
+
+      purchase(amount_in_cents, source, options.merge(payment_intent_options: payment_intent_options))
+    end
+
     # Captures a certain amount from a previously authorized transaction.
     #
     # @see https://stripe.com/docs/api/payment_intents/capture#capture_payment_intent
