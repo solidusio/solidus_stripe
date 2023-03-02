e11377ff27861f6a29ef23371379431e2d727a0a
Elia Schito <elia@schito.me>
2023-03-02 16:42:59 +0100

    Add PaymentSource#stripe_payment_method
    Fetch the Stripe object to be used for reusing sources.

--- app/models/solidus_stripe/payment_source.rb
+++ app/models/solidus_stripe/payment_source.rb
@@ -4,5 +4,10 @@ require 'stripe'
 
 module SolidusStripe
   class PaymentSource < ::Spree::PaymentSource
+    def stripe_payment_method
+      return if stripe_payment_method_id.blank?
+
+      @stripe_payment_method ||= payment_method.gateway.request { Stripe::PaymentMethod.retrieve(stripe_payment_method_id) }
+    end
   end
 end
