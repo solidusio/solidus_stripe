c04a24dc5b99cbc10136cb8dc69d9959e14c4bf3
Elia Schito <elia@schito.me>
2023-03-10 17:45:22 +0100

    Add factories for payment and setup intent models
    Co-Authored-By: Alberto Vena <kennyadsl@gmail.com>

--- lib/solidus_stripe/testing_support/factories.rb
+++ lib/solidus_stripe/testing_support/factories.rb
@@ -17,4 +17,14 @@ FactoryBot.define do
     association :payment_method, factory: :stripe_payment_method
     stripe_payment_method_id { "pm_#{SecureRandom.uuid.delete('-')}" }
   end
+
+  factory :stripe_payment_intent, class: 'SolidusStripe::PaymentIntent' do
+    association :order
+    stripe_payment_intent_id { "pm_#{SecureRandom.uuid.delete('-')}" }
+  end
+
+  factory :stripe_setup_intent, class: 'SolidusStripe::SetupIntent' do
+    association :order
+    stripe_setup_intent_id { "seti_#{SecureRandom.uuid.delete('-')}" }
+  end
 end
