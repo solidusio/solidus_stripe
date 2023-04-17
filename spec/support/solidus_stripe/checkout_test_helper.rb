977b49959cb4fc8ec457e898c924ca4e34478a35
Rainer Dema <rainer.dema@gmail.com>
2023-04-17 19:17:11 +0200

    Uniform helper method names in checkout specs

--- spec/support/solidus_stripe/checkout_test_helper.rb
+++ spec/support/solidus_stripe/checkout_test_helper.rb
@@ -13,7 +13,7 @@ module SolidusStripe::CheckoutTestHelper
   # These are methods that are used specifically for setting up the
   # environment for testing.
 
-  def assigns_guest_token(guest_token)
+  def assign_guest_token(guest_token)
     # rubocop:disable RSpec/AnyInstance
     allow_any_instance_of(ActionDispatch::Cookies::SignedKeyRotatingCookieJar).tap do |allow_cookie_jar|
       # Retrieve all other cookies from the original jar.
@@ -23,7 +23,7 @@ module SolidusStripe::CheckoutTestHelper
     # rubocop:enable RSpec/AnyInstance
   end
 
-  def creates_payment_method(setup_future_usage: 'off_session', auto_capture: false)
+  def create_payment_method(setup_future_usage: 'off_session', auto_capture: false)
     @payment_method = create(
       :stripe_payment_method,
       preferred_setup_future_usage: setup_future_usage,
@@ -44,7 +44,7 @@ module SolidusStripe::CheckoutTestHelper
     current_order.payments.reorder(id: :desc).find_by(source_type: "SolidusStripe::PaymentSource")
   end
 
-  def captures_last_valid_payment
+  def capture_last_valid_payment
     payment = current_order.payments.valid.last
     payment.capture!
     expect(payment.payment_method.type).to eq('SolidusStripe::PaymentMethod')
@@ -59,7 +59,7 @@ module SolidusStripe::CheckoutTestHelper
   # These are methods that are used specifically for interacting with
   # the Stripe payment form.
 
-  def fills_stripe_form(
+  def fill_stripe_form(
     number: 4242_4242_4242_4242, # rubocop:disable Style/NumericLiterals
     expiry_month: 12,
     expiry_year: Time.current.year + 1,
@@ -68,18 +68,18 @@ module SolidusStripe::CheckoutTestHelper
     country: 'United States',
     zip: '90210'
   )
-    fills_in_stripe_cvc(cvc)
-    fills_in_stripe_expiry_date(year: expiry_year, month: expiry_month, date: date)
-    fills_in_stripe_card(number)
-    fills_in_stripe_country(country)
-    fills_in_stripe_zip(zip) if zip # not shown for every country
+    fill_in_stripe_cvc(cvc)
+    fill_in_stripe_expiry_date(year: expiry_year, month: expiry_month, date: date)
+    fill_in_stripe_card(number)
+    fill_in_stripe_country(country)
+    fill_in_stripe_zip(zip) if zip # not shown for every country
   end
 
-  def fills_in_stripe_card(number)
+  def fill_in_stripe_card(number)
     fills_in_stripe_input 'number', with: number
   end
 
-  def fills_in_stripe_expiry_date(year: nil, month: nil, date: nil)
+  def fill_in_stripe_expiry_date(year: nil, month: nil, date: nil)
     date ||= begin
       month = month.to_s.rjust(2, '0') unless month.is_a? String
       year = year.to_s[2..3] unless year.is_a? String
@@ -89,34 +89,34 @@ module SolidusStripe::CheckoutTestHelper
     fills_in_stripe_input 'expiry', with: date.to_s[0..3]
   end
 
-  def fills_in_stripe_cvc(cvc)
+  def fill_in_stripe_cvc(cvc)
     fills_in_stripe_input 'cvc', with: cvc.to_s[0..2].to_s
   end
 
-  def fills_in_stripe_country(country_name)
+  def fill_in_stripe_country(country_name)
     using_wait_time(10) do
-      within_frame(finds_stripe_iframe) do
+      within_frame(find_stripe_iframe) do
         find(%{select[name="country"]}).select(country_name)
       end
     end
   end
 
-  def fills_in_stripe_zip(zip)
+  def fill_in_stripe_zip(zip)
     fills_in_stripe_input 'postalCode', with: zip
   end
 
   def fills_in_stripe_input(name, with:)
     using_wait_time(10) do
-      within_frame(finds_stripe_iframe) do
+      within_frame(find_stripe_iframe) do
         with.to_s.chars.each { find(%{input[name="#{name}"]}).send_keys(_1) }
       end
     end
   end
 
-  def clears_stripe_form
+  def clear_stripe_form
     %w[number expiry cvc postalCode].each do |name|
       using_wait_time(10) do
-        within_frame(finds_stripe_iframe) do
+        within_frame(find_stripe_iframe) do
           field = find(%{input[name="#{name}"]})
           field.value.length.times { field.send_keys [:backspace] }
         end
@@ -124,7 +124,7 @@ module SolidusStripe::CheckoutTestHelper
     end
   end
 
-  def finds_stripe_iframe
+  def find_stripe_iframe
     fieldset = find_payment_fieldset(payment_method.id)
     expect(fieldset).to have_css('iframe', wait: 15) # trigger waiting if the frame is not yet there
     fieldset.find("iframe")
@@ -142,25 +142,25 @@ module SolidusStripe::CheckoutTestHelper
   # More information on this property can be found in the Stripe API documentation:
   # PaymentIntent objects : https://stripe.com/docs/api/payment_intents/object#payment_intent_object-next_action
 
-  def authorizes_3d_secure_payment(authenticate: true)
-    finds_frame('body > div > iframe') do
-      finds_frame('#challengeFrame') do
-        finds_frame("iframe[name='acsFrame']") do
+  def authorize_3d_secure_payment(authenticate: true)
+    find_frame('body > div > iframe') do
+      find_frame('#challengeFrame') do
+        find_frame("iframe[name='acsFrame']") do
           click_on authenticate ? 'Complete authentication' : 'Fail authentication'
         end
       end
     end
   end
 
-  def authorizes_3d_secure_2_payment(authenticate: true)
-    finds_frame('body > div > iframe') do
-      finds_frame('#challengeFrame') do
+  def authorize_3d_secure_2_payment(authenticate: true)
+    find_frame('body > div > iframe') do
+      find_frame('#challengeFrame') do
         click_on authenticate ? 'Complete' : 'Fail'
       end
     end
   end
 
-  def finds_frame(selector, &block)
+  def find_frame(selector, &block)
     using_wait_time(15) do
       frame = find(selector)
       within_frame(frame, &block)
@@ -172,47 +172,47 @@ module SolidusStripe::CheckoutTestHelper
   # These are methods that are used specifically for interacting with
   # the checkout process.
 
-  def visits_payment_step(user: nil)
+  def visit_payment_step(user: nil)
     @current_order = Spree::TestingSupport::OrderWalkthrough.up_to(:delivery, user: user)
 
     if user
       sign_in current_order.user
     else
-      assigns_guest_token current_order.guest_token
+      assign_guest_token current_order.guest_token
     end
 
     visit '/checkout/payment'
   end
 
-  def chooses_new_stripe_payment
+  def choose_new_stripe_payment
     choose(option: payment_method.id)
   end
 
-  def submits_payment
+  def submit_payment
     click_button("Save and Continue")
   end
 
-  def checks_terms_of_service
+  def check_terms_of_service
     expect(page).to have_content("Agree to Terms of Service")
     check "Agree to Terms of Service"
   end
 
-  def confirms_order
+  def confirm_order
     click_button("Place Order")
   end
 
-  def completes_order
-    checks_terms_of_service
-    confirms_order
+  def complete_order
+    check_terms_of_service
+    confirm_order
     expect(page).to have_content('Your order has been processed successfully')
   end
 
-  def moves_order_back_to_payment
+  def expects_page_and_order_to_be_in_payment_step
     expect(page).to have_current_path('/checkout/payment')
     expect(current_order.state).to eq('payment')
   end
 
-  def fails_the_payment
+  def expects_payment_to_be_failed
     expect(last_stripe_payment.state).to eq('failed')
   end
 
@@ -258,18 +258,18 @@ module SolidusStripe::CheckoutTestHelper
   end
 
   def invalid_data_are_notified
-    fills_in_stripe_country('United States')
+    fill_in_stripe_country('United States')
 
     [
       [{ number: '4242424242424241' }, 'Your card number is invalid'],  # incorrect_number
       [{ date: '1110' }, "Your card's expiration year is in the past"], # invalid_expiry_year
       [{ cvc: 99 }, "Your card's security code is incomplete"]          # invalid_cvc
     ].each do |args, text|
-      clears_stripe_form
-      fills_stripe_form(**args)
-      submits_payment
+      clear_stripe_form
+      fill_stripe_form(**args)
+      submit_payment
       using_wait_time(10) do
-        within_frame(finds_stripe_iframe) do
+        within_frame(find_stripe_iframe) do
           expect(page).to have_content(text)
         end
       end
@@ -280,17 +280,17 @@ module SolidusStripe::CheckoutTestHelper
     # In order to have a complete Stripe form,
     # it's essential to have a Postal Code field that will only be displayed
     # when the user selects United States as their country.
-    fills_in_stripe_country('United States')
+    fill_in_stripe_country('United States')
 
-    clears_stripe_form
-    submits_payment
+    clear_stripe_form
+    submit_payment
     [
       "Your card number is incomplete",
       "Your card's expiration date is incomplete",
       "Your card's security code is incomplete",
       "Your postal code is incomplete"
     ].each do |text|
-      within_frame(finds_stripe_iframe) do
+      within_frame(find_stripe_iframe) do
         expect(page).to have_content(text)
       end
     end
@@ -312,23 +312,23 @@ module SolidusStripe::CheckoutTestHelper
       # https://stripe.com/docs/testing#fraud-prevention
       ['4100000000000019', 'Your card has been declined.'],                  # Always blocked
     ].each do |number, text|
-      fills_in_stripe_country('United States')
-      fills_stripe_form(number: number)
-      submits_payment
-      checks_terms_of_service
-      confirms_order
+      fill_in_stripe_country('United States')
+      fill_stripe_form(number: number)
+      submit_payment
+      check_terms_of_service
+      confirm_order
       expect(page).to have_content(text, wait: 15)
     end
   end
 
   def successfully_creates_a_payment_intent(user: nil, auto_capture: false)
-    visits_payment_step(user: user)
-    chooses_new_stripe_payment
-    fills_stripe_form
+    visit_payment_step(user: user)
+    choose_new_stripe_payment
+    fill_stripe_form
 
-    submits_payment
+    submit_payment
 
-    completes_order
+    complete_order
 
     if auto_capture
       payment_intent_is_created_and_successfully_captured
