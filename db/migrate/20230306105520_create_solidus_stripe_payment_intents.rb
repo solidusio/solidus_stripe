2b3aacde5abe164d68200d74225192df00ec0e24
Elia Schito <elia@schito.me>
2023-04-27 18:58:59 +0200

    Compact migrations
    One migration per table

--- db/migrate/20230306105520_create_solidus_stripe_payment_intents.rb
+++ db/migrate/20230306105520_create_solidus_stripe_payment_intents.rb
@@ -1,9 +1,9 @@
 class CreateSolidusStripePaymentIntents < ActiveRecord::Migration[7.0]
   def change
     create_table :solidus_stripe_payment_intents do |t|
-      t.string :stripe_payment_intent_id
+      t.string :stripe_intent_id
       t.references :order, null: false, foreign_key: { to_table: :spree_orders }
-
+      t.references :payment_method, null: false, foreign_key: { to_table: :spree_payment_methods }
       t.timestamps
     end
   end
