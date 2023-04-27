1a83c5dd038e343f97e30d7bfec94d2fc29b82ec
Rainer Dema <rainer.dema@gmail.com>
2023-04-27 14:41:48 +0200

    Allow the use of non-card Stripe payment methods
    This update ensures that the Payment Intent created is compatible with
    the created Stripe Payment Method from Stripe Elements by allowing for
    identification and use of activated payment methods within the Stripe
    dashboard.
    This prevents any issues with compatibility when attaching a non-card
    Stripe Payment Method with the new Stripe Payment Intent.
    When retrieving or creating a Payment Intent, the payment_method_type [1]
    of the Stripe payment method will always be included for compatibility
    assurance.
    
    Why we're not enabling the automatic_payment_methods [2]?
    The automatic_payment_methods parameter is not being enabled due to
    potential issues with existing payment sources in admin and in general
    in case of using authorize/purchase from the Gateway.
    Enabling this parameter would require a mandatory `return_url` during
    confirmation, which poses problems on the admin-side where confirmation
    and authorize/purchase are performed on the Gateway without the
    possibility of specifying a `return_url`.
    The case is different for the frontend-side, where the confirmation
    and authorization occur on the client-side (and where we specify the
    `return_url`).
    
    The error message received in this scenario is:
    "You must provide a return_url when confirming using automatic_payment_methods[enabled]=true."
    
    In addition to addressing compatibility issues, this change will also
    resolve #263 by enabling the possibility to specify supported payment
    method types directly during the initialization of Stripe Elements.
    
    [1] https://stripe.com/docs/api/payment_intents/object#payment_intent_object-payment_method_types
    [2] https://stripe.com/docs/api/payment_intents/object#payment_intent_object-automatic_payment_methods-enabled

--- app/models/solidus_stripe/payment_intent.rb
+++ app/models/solidus_stripe/payment_intent.rb
@@ -5,17 +5,9 @@ module SolidusStripe
     belongs_to :order, class_name: 'Spree::Order'
     belongs_to :payment_method, class_name: 'SolidusStripe::PaymentMethod'
 
-    def self.prepare_for_payment(payment, **stripe_intent_options)
+    def self.prepare_for_payment(payment, **stripe_creation_options)
       # Find or create the intent for the payment.
-      intent =
-        retrieve_last_usable_intent(payment) ||
-          new(payment_method: payment.payment_method, order: payment.order)
-            .tap { _1.update!(stripe_intent_id: _1.create_stripe_intent(**stripe_intent_options).id) }
-
-      # Update the intent with the previously acquired payment method.
-      intent.payment_method.gateway.request {
-        Stripe::PaymentIntent.update(intent.stripe_intent_id, payment_method: payment.source.stripe_payment_method_id)
-      }
+      intent = retrieve_for_payment(payment) || create_for_payment(payment, **stripe_creation_options)
 
       # Attach the payment intent to the payment.
       payment.update!(response_code: intent.stripe_intent.id)
@@ -23,9 +15,26 @@ module SolidusStripe
       intent
     end
 
-    def self.retrieve_last_usable_intent(payment)
+    def self.create_for_payment(payment, **stripe_intent_options)
+      new(payment_method: payment.payment_method, order: payment.order)
+        .tap { _1.update!(stripe_intent_id: _1.create_stripe_intent(payment, **stripe_intent_options).id) }
+    end
+
+    def self.retrieve_for_payment(payment)
       intent = where(payment_method: payment.payment_method, order: payment.order).last
-      intent if intent&.usable?
+
+      return unless intent&.usable?
+
+      # Update the intent with the previously acquired payment method.
+      intent.payment_method.gateway.request {
+        Stripe::PaymentIntent.update(
+          intent.stripe_intent_id,
+          payment_method: payment.source.stripe_payment_method_id,
+          payment_method_types: [payment.source.stripe_payment_method.type],
+        )
+      }
+
+      intent
     end
 
     def usable?
@@ -89,7 +98,7 @@ module SolidusStripe
       super
     end
 
-    def create_stripe_intent(**stripe_intent_options)
+    def create_stripe_intent(payment, **stripe_intent_options)
       stripe_customer_id = SolidusStripe::Customer.retrieve_or_create_stripe_customer_id(
         payment_method: payment_method,
         order: order
@@ -102,6 +111,8 @@ module SolidusStripe
           capture_method: payment_method.auto_capture? ? 'automatic' : 'manual',
           setup_future_usage: payment_method.preferred_setup_future_usage.presence,
           customer: stripe_customer_id,
+          payment_method: payment.source.stripe_payment_method_id,
+          payment_method_types: [payment.source.stripe_payment_method.type],
           metadata: { solidus_order_number: order.number },
           **stripe_intent_options,
         })
