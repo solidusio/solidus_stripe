6be4a594ae26fbf12afd58b0f56a85fbef3ffcb8
Elia Schito <elia@schito.me>
2023-04-28 13:11:13 +0200

    Inline backend spec helpers related to flash messages and payment status
    Turned out reusability was pretty low.
    
    Co-Authored-By: Rainer Dema <rainer.dema@gmail.com>

--- spec/system/backend/solidus_stripe/orders/payments_spec.rb
+++ spec/system/backend/solidus_stripe/orders/payments_spec.rb
@@ -11,7 +11,8 @@ RSpec.describe 'SolidusStripe Orders Payments', :js do
       payment = create_authorized_payment
       visit_payments_page
 
-      expects_page_to_display_payment(payment)
+      click_on 'Payments'
+      expect(page).to have_content(payment.number)
     end
 
     it 'displays log entries for a payment' do
@@ -28,7 +29,9 @@ RSpec.describe 'SolidusStripe Orders Payments', :js do
 
       capture_payment
 
-      expects_page_to_display_successfully_captured_payment(payment)
+      expect(page).to have_content('Payment Updated')
+      expect(payment.reload.state).to eq('completed')
+      expect(payment.capture_events.first.amount).to eq(payment.amount)
       expects_payment_to_have_correct_capture_amount_on_stripe(payment, payment.amount)
     end
 
@@ -40,7 +43,9 @@ RSpec.describe 'SolidusStripe Orders Payments', :js do
 
       capture_partial_payment_amount(payment, partial_capture_amount)
 
-      expects_page_to_display_successfully_captured_payment(payment)
+      expect(page).to have_content('Payment Updated')
+      expect(payment.reload.state).to eq('completed')
+      expect(payment.capture_events.first.amount).to eq(payment.amount)
       expects_payment_to_have_correct_capture_amount_on_stripe(payment, partial_capture_amount)
     end
 
@@ -50,7 +55,8 @@ RSpec.describe 'SolidusStripe Orders Payments', :js do
 
       void_payment
 
-      expects_page_to_display_successfully_voided_payment(payment)
+      expect(page).to have_content('Payment Updated')
+      expect(payment.reload.state).to eq('void')
       expects_payment_to_be_voided_on_stripe(payment)
     end
 
@@ -60,7 +66,8 @@ RSpec.describe 'SolidusStripe Orders Payments', :js do
 
       refund_payment
 
-      expects_page_to_display_successfully_refunded_payment(payment)
+      expect(page).to have_content('Refund has been successfully created!')
+      expect(payment).to be_fully_refunded
       expects_payment_to_be_refunded_on_stripe(payment, payment.amount)
     end
 
@@ -72,7 +79,9 @@ RSpec.describe 'SolidusStripe Orders Payments', :js do
       partial_refund_amount = 25
       partially_refund_payment(refund_reason, partial_refund_amount)
 
-      expects_page_to_display_successfully_partially_refunded_payment(payment, partial_refund_amount)
+      expect(page).to have_content('Refund has been successfully created!')
+      expect(payment.reload.state).to eq('completed')
+      expect(payment.refunds.first.amount).to eq(partial_refund_amount)
       expects_payment_to_be_refunded_on_stripe(payment, partial_refund_amount)
     end
 
@@ -85,7 +94,8 @@ RSpec.describe 'SolidusStripe Orders Payments', :js do
       # https://github.com/solidusio/solidus/blob/ab59d6435239b50db79d73b9a974af057ad56b52/core/app/models/spree/payment_method.rb#L169-L181
       pending "needs to implement try_void method to handle voiding payments on order cancellation"
 
-      expects_page_to_display_successfully_canceled_order_payment(payment)
+      expect(page).to have_content('Order canceled')
+      expect(payment.reload.state).to eq('void')
       expects_payment_to_be_voided_on_stripe(payment)
     end
 
@@ -100,7 +110,8 @@ RSpec.describe 'SolidusStripe Orders Payments', :js do
       # with locating the user who canceled the order.
       pending "needs to implement try_void method to handle voiding payments on order cancellation"
 
-      expects_page_to_display_successfully_canceled_order_payment(payment)
+      expect(page).to have_content('Order canceled')
+      expect(payment.reload.state).to eq('void')
       expects_payment_to_be_voided_on_stripe(payment)
     end
 
@@ -112,7 +123,9 @@ RSpec.describe 'SolidusStripe Orders Payments', :js do
       capture_payment
       payment = last_valid_payment
 
-      expects_page_to_display_successfully_captured_payment(payment)
+      expect(page).to have_content('Payment Updated')
+      expect(payment.reload.state).to eq('completed')
+      expect(payment.capture_events.first.amount).to eq(payment.amount)
       expects_payment_to_have_correct_capture_amount_on_stripe(payment, payment.amount)
     end
   end
@@ -124,8 +137,10 @@ RSpec.describe 'SolidusStripe Orders Payments', :js do
 
       capture_payment
 
-      expects_page_to_display_capture_fail_message(payment,
-        'This PaymentIntent could not be captured because it has a status of requires_action.')
+      expect(page).to have_content(
+        'This PaymentIntent could not be captured because it has a status of requires_action.'
+      )
+      expect(payment.reload.state).to eq('failed')
     end
   end
 end
