c8528ba1d258577ad117adbafab45776c1f37c78
Elia Schito <elia@schito.me>
2023-04-28 21:50:11 +0200

    Manually add foreign keys in migrations
    MySQL had an issue with doing the same with `t.references`.

--- db/migrate/20230306105520_create_solidus_stripe_payment_intents.rb
+++ db/migrate/20230306105520_create_solidus_stripe_payment_intents.rb
@@ -2,9 +2,13 @@ class CreateSolidusStripePaymentIntents < ActiveRecord::Migration[7.0]
   def change
     create_table :solidus_stripe_payment_intents do |t|
       t.string :stripe_intent_id
-      t.references :order, null: false, foreign_key: { to_table: :spree_orders }
-      t.references :payment_method, null: false, foreign_key: { to_table: :spree_payment_methods }
+      t.integer :order_id, null: false, index: true
+      t.integer :payment_method_id, null: false, index: true
+
       t.timestamps
+
+      t.foreign_key :spree_orders, column: :order_id
+      t.foreign_key :spree_payment_methods, column: :payment_method_id
     end
   end
 end
--- db/migrate/20230308122414_create_solidus_stripe_slug_entries.rb
+++ db/migrate/20230308122414_create_solidus_stripe_slug_entries.rb
@@ -1,10 +1,12 @@
 class CreateSolidusStripeSlugEntries < ActiveRecord::Migration[5.2]
   def change
     create_table :solidus_stripe_slug_entries do |t|
-      t.references :payment_method, null: false, foreign_key: { to_table: :spree_payment_methods }
+      t.integer :payment_method_id, null: false, index: true
       t.string :slug, null: false, index: { unique: true }
 
       t.timestamps
+
+      t.foreign_key :spree_payment_methods, column: :payment_method_id
     end
   end
 end
