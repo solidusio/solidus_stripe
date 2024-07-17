c7cd371ba5aefa0ffc9f904057c36cd4b91da92e
Kamil Chodorek <kamil.chodorek@upsidelab.io>
2024-07-17 14:28:11 +0200

    Refactor Stripe API controller to use Service Objects

--- app/controllers/spree/api/stripe_controller.rb
+++ app/controllers/spree/api/stripe_controller.rb
@@ -7,14 +7,21 @@ module Spree
         payment_method = SolidusStripe::PaymentMethod.active.last
         customer = SolidusStripe::Customer.find_or_create_by!(source: user, payment_method: payment_method)
 
-        if customer.stripe_id.blank?
-          stripe_customer = customer.create_stripe_customer
-          customer.update!(stripe_id: stripe_customer.id)
-        end
+        create_stripe_customer(customer) if customer.stripe_id.blank?
 
         intent = payment_method.gateway.request { Stripe::SetupIntent.create({ customer: CGI.escape(customer.stripe_id) }) }
 
+
+        intent = Stripe::AddPaymentMethodIntentForUser.new.call(user: current_user)
+
         render json: { client_secret: intent['client_secret'] }
+      end
+
+      private
+
+      def create_stripe_customer(customer)
+        stripe_customer = customer.create_stripe_customer
+        customer.update!(stripe_id: stripe_customer.id)
       end
     end
   end
